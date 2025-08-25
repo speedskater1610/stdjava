be0273a6b22da2fa413943932c3bf70187341973
speedskater1610 <speedskater1610@gmail.com>
2025-08-25 16:35:58 -0400

    Update Js.java

--- src/langs/Js.java
+++ src/langs/Js.java
@@ -1,1 +1,26 @@
+package langs;
 
+
+import org.graalvm.polyglot.*;
+
+public class 
+Js 
+{
+    public static void 
+    js(String code) 
+    {
+        
+        // I am going to be completly honest when I say I have no idea what any of this does 
+        // thank the lord for stack overflow
+        try 
+        (
+            Context context = Context.newBuilder("js")
+                .allowAllAccess(true) // java interop
+                .out(System.out)      // js console output to java console
+                .build()
+        ) 
+        {
+            context.eval("js", code);
+        }
+    }
+}
