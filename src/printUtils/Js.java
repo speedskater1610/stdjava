9f19887e757457a00f2adce4be15d04e27f9c0cd
speedskater1610 <speedskater1610@gmail.com>
2025-08-25 20:16:01 -0400

    Create Js.java

---
+++ src/printUtils/Js.java
@@ -0,0 +1,17 @@
+package printUtils; 
+
+public class 
+Js 
+{
+    // so console.log
+    public static class 
+    console
+    {
+        public static void 
+        log (String string) 
+        {
+            System.out.println(string);
+        }
+    }
+    
+}
